5c5d4c51d18c78862aa54f4b04243c9cfcf17b8f
RanjithKumarKarnan <ranjithkarnan2002@gmail.com>
2026-03-12 20:16:35 +0530

    Feat : updation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,14 @@
- const menuBtn = document.getElementById("menu-toggle");
-  const mobileMenu = document.getElementById("mobile-menu");
+const menuBtn = document.getElementById("menu-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
 
-  if (menuBtn) {
-    menuBtn.addEventListener("click", () => {
-      mobileMenu.classList.toggle("hidden");
-    });
-  }
-
-  document.querySelectorAll("#mobile-menu a").forEach(link => {
-    link.addEventListener("click", () => {
-      mobileMenu.classList.add("hidden");
-    });
+if (menuBtn) {
+  menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
   });
+}
+
+document.querySelectorAll("#mobile-menu a").forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.add("hidden");
+  });
+});
